d32d78eb6a975a339ead61abcbc4f9b987c8ba76
axrorback <axacyber1@gmail.com>
2025-12-19 16:28:29 +0500

    test: pair programming example
    Co-authored-by: Asadback25 <Asadback25@gmail.com>

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,33 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+# from users.models import CustomUser
+# from tasks.models import Task
+# import uuid
+# from django.core.validators import RegexValidator
+#
+# project_code_validator = RegexValidator(regex=r'^CB25PRJ-[0-9]{4}$',message='Project code formati CB25PRJ-0001 kabi bolishi kerak!')
+#
+# STATUS_CHOICES = (
+#     ('done','Done'),
+#     ('in_progress','In Progress'),
+#     ('pending','Pending'),
+#     ('cancelled','Cancelled'),
+#     ('rejected','Rejected'),
+#     ('approved','Approved'),
+#     ('failed','Failed')
+# )
+#
+#
+# SERVER_STATUS_CHOICES = (
+#     ('deployed','Deployed'),
+#     ('not_deployed','Not Deployed'),
+#     ('disabled','Disabled'),
+#     ('active','Active'),
+# )
+#
+# class Project(models.Model):
+#     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+#     code = models.CharField(max_length=15,unique=True,validators=[project_code_validator])
+#     status_coding = models.CharField(max_length=12,choices=STATUS_CHOICES,default='pending')
+#     status_deployment = models.CharField(max_length=12,choices=SERVER_STATUS_CHOICES,default='pending')
+#     created_by = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='created_projects')
+#
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList , TaskDetail
 
 urlpatterns = [
     path('list/', TaskList.as_view()),
+    path('detail/<uuid:pk>/',TaskDetail.as_view())
 ]
