475170d89dacefa3eb49ab5877bcf9f882974880
axrorback <axrorback@gmail.com>
2026-03-12 03:36:58 +0500

    Enable Celery broker connection retry on start

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
+CELERY_BROKER_CONNECTION_RETRY_ON_START = True
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
