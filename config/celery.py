5b0c159c96ac7c8f603c4e6effe946129f27570a
axrorback <axrorback@gmail.com>
2026-03-14 22:59:58 +0500

    Improve Celery worker concurrency and ensure database connections are managed via signal handlers

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,19 @@
-from __future__ import absolute_import , unicode_literals
-from celery import Celery
+from __future__ import absolute_import, unicode_literals
 import os
+from celery import Celery
+from celery.signals import task_prerun, task_postrun
+from django.db import close_old_connections
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+@task_prerun.connect
+def celery_prerun(*args, **kwargs):
+    close_old_connections()
+
+@task_postrun.connect
+def celery_postrun(*args, **kwargs):
+    close_old_connections()
