33f0f9a60153f7e0efc1853622a9a1e1bc5496b9
Ahrorjon Ibrohimjonov <axrorback@gmail.com>
2026-03-15 12:37:14 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ PASSWORD_HASHERS = [
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True
     )
 }
